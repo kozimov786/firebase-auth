9f16963c423fe2e1defc2b908a9f0396bd6fe0e7
kozimov786 <email@youremail.com>
2022-01-30 12:33:57 +0500

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNKXbzLpJUP4JfVLFTPChV2ITVQqqgr_c",
+  authDomain: "auth-develoment-1584e.firebaseapp.com",
+  projectId: "auth-develoment-1584e",
+  storageBucket: "auth-develoment-1584e.appspot.com",
+  messagingSenderId: "93385969812",
+  appId: "1:93385969812:web:23ce212fdad3bc674de481"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
